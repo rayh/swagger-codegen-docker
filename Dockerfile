ece666eaa7b56371a2aeae87e68fa5e9d9d1f4ee
Ray Hilton <ray.hilton@intelematics.com>
2015-12-22 15:58:41 +1100

    Update generator to support asciidoc and markdown

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,14 @@ RUN apt-get update -q
 RUN apt-get install -y -q maven
 
 RUN git clone https://github.com/swagger-api/swagger-codegen.git
-WORKDIR swagger-codegen
-RUN mvn package
+RUN cd swagger-codegen && mvn package
 
 RUN apt-get install -y zip
 
+RUN git clone https://github.com/Swagger2Markup/swagger2markup-cli.git
+RUN cd swagger2markup-cli && bash gradlew build
+
+WORKDIR /scripts
 ADD generate.sh generate.sh
 
-
-ENTRYPOINT ["/swagger-codegen/generate.sh"]
+ENTRYPOINT ["/scripts/generate.sh"]
--- generate.sh
+++ generate.sh
@@ -8,10 +8,21 @@ NAME=$(echo $INPUT | sed -e 's/\.yml$//g' | sed -e 's/\.json$//g' )
 mkdir artifacts/ output/
 
 echo "Generate spec $INPUT for lang $LANG"
-java -jar /swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+
+case $LANG in
+asciidoc | markdown)
+  java -jar /swagger2markup-cli/build/libs/swagger2markup-cli-0.9.1.jar generate \
   -i $INPUT \
   -l $LANG \
   -o output/$NAME/$LANG/
+;;
+*)
+  java -jar /swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+  -i $INPUT \
+  -l $LANG \
+  -o output/$NAME/$LANG/
+;;
+esac
 
 cd output/$NAME/$LANG
 zip -r ../../swagger-$NAME-$LANG-$TAG.zip .
